833cef34cd0f6f2a7d6f259060305732615d8dc6
WilliamVargasMedeiros <william.vargasmedeiros@gmail.com>
2022-04-21 02:52:39 -0300

    Criação do projeto

---
+++ Projeto Java - Curso Java Web Full-Stack/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiroClasseJava {
+
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		System.out.println("meu primeiro programa em Java");
+		System.out.println("Java é orientado a objetos");
+		
+	}
+}
