b279a852a0730cb99bec717ce7982937de768f1e
WilliamVargasMedeiros <william.vargasmedeiros@gmail.com>
2022-04-21 22:17:22 -0300

    Alteração da classe java

--- Projeto Java - Curso Java Web Full-Stack/src/curso/java/william/Pessoa.java
+++ Projeto Java - Curso Java Web Full-Stack/src/curso/java/william/Pessoa.java
@@ -12,12 +12,14 @@ public class Pessoa {
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getSobrenome() {
 		return sobrenome;
 	}
